658c729eeb3c42d665412ea1b4c6f5f7936dc3fe
merycicek <merycicek@gmail.com>
2020-09-11 14:14:25 -0500

    change the credentials

---
+++ deployments/JenkinsDeploy.groovy
@@ -0,0 +1,18 @@
+properties([
+    [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
+    parameters([
+        booleanParam(defaultValue: false, description: 'Please select to apply all changes to the environment', name: 'applyChanges'), 
+        booleanParam(defaultValue: false, description: 'Please select to destroy all changes to the environment', name: 'DestroyChanges'), 
+        string(defaultValue: '', description: 'Please provide the docker to deploy', name: 'selectedDockerimage', trim: false), 
+        choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please provide the environment to deploy ', name: 'environment')
+        ])
+        ])
+println(
+    """
+    Apply changes: ${params.applyChanges}
+    Destroy changes: ${params.destroyChanges}
+    Docker image: ${params.selectedDockerImage}
+    Environment: ${params.environment}
+    """
+
+)        
---
+++ deployments/jenkinsBuilder.groovy
@@ -0,0 +1,86 @@
+// Uniq name for the pod or slave 
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+def branch = "${scm.branches[0].name}".replaceAll(/^\*\//, '')
+def gitCommitHash
+
+
+properties([
+    [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
+    parameters([
+        booleanParam(defaultValue: false, description: 'Select to be able to psuh to latest ', name: 'pushLatest')
+        ])
+    ])
+
+    
+// yaml def for slaves 
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: docker
+          image: docker:latest
+          imagePullPolicy: IfNotPresent
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+    podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+      node(k8slabel) {
+
+        stage("Checkout SCM") {
+            checkout scm 
+        }
+
+        dir('deployments/docker') {
+            container('docker') {
+                withCredentials([usernamePassword(credentialsId: 'docker-creds', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+
+                    stage('Docker Build') {
+                        sh 'docker build -t artemis .'
+                    }
+
+                    stage('Docker Login') {
+                        sh "docker login --username  $USERNAME  --password $PASSWORD " 
+                    }
+
+                    stage('Docker Push') {
+
+                        if (params.pushLatest) {
+                            println('Pushing the image to latest version!!')
+                            sh "docker tag artemis merycicek/artemis:latest"
+                            sh "docker push merycicek/artemis:latest"
+                        } 
+                        
+                        sh "docker tag artemis merycicek/artemis:${branch}"
+                        sh "docker push merycicek/artemis:${branch}"
+                    }
+                }
+            }
+        }
+      }
+    }
